200a717365483c2cb75769543fed65ae24233684
rharshita21 <raghav.harshita21@gmail.com>
2023-04-02 23:58:56 +0530

    Updated Form

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>User Dashboard</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <h1>Welcome Dashboard</h1>
+        <h1 class="form-heading">Welcome to Dashboard</h1>
+        <a href="logout.php" class="btn btn-warning">Logout</a>
     </div>
-    
 </body>
 </html>
--- login.php
+++ login.php
@@ -15,7 +15,38 @@ if (isset($_SESSION["user"])) {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 9px 14px 10px 14px">
+  <a class="navbar-brand" href="#">29Kreativ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="./registration.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+   
+    </ul>
+    <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+  </div>
+</nav>
+
     <div class="container">
+      <h1 class="form-heading">Please Login</h1>
         <?php
         if (isset($_POST["login"])) {
            $email = $_POST["email"];
@@ -49,7 +80,7 @@ if (isset($_SESSION["user"])) {
             <input type="submit" value="Login" name="login" class="btn btn-primary">
         </div>
       </form>
-     <div><p>Not registered yet <a href="registration.php">Register Here</a></p></div>
+     <div><p>Not registered yet? <a href="registration.php">Register Here</a></p></div>
     </div>
 </body>
 </html>
